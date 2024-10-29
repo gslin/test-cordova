d4fce5c72547048b126ae38694a045aec01fb554
Gea-Suan Lin <gslin@gslin.org>
2024-10-30 05:31:03 +0800

    feat: use axios to call API.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import axios from 'axios';
+
 const main = async () => {
-  const res = await fetch('https://www.example.com/');
+  const res = await axios.get('https://www.example.com/');
 
   const el = document.querySelector('textarea');
-  el.innerText = await res.text();
+  el.innerText = await res.data;
 };
 
 document.addEventListener('deviceready', main, false);
