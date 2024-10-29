17c98e7e9016f59c7261b09947b870bb592f4367
Gea-Suan Lin <gslin@gslin.org>
2024-10-30 05:21:21 +0800

    feat: try to access www.example.com.

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,8 @@
-const main = () => {
+const main = async () => {
+  const res = await fetch('https://www.example.com/');
+
+  const el = document.querySelector('textarea');
+  el.innerText = await res.text();
 };
 
 document.addEventListener('deviceready', main, false);
