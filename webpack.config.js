3a137e0f70b40ef5beff582f852da5b09b666127
Gea-Suan Lin <gslin@gslin.org>
2024-10-30 16:24:27 +0800

    feat: use vue-loader for *.vue.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
   mode: 'development',
+  module: {
+    rules: [{
+      test: /\.vue$/,
+      use: 'vue-loader',
+    }],
+  },
   output: {
     path: path.resolve(__dirname, 'www/js'),
   },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
 };
