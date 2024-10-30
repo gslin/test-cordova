0af1bf0ca4dba1eeb00952277ad1566ca22cfde4
Gea-Suan Lin <gslin@gslin.org>
2024-10-30 18:13:32 +0800

    refactor: change to use ref and avoid from the use of onMounted().

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,23 +4,24 @@
     <hr>
     <h2>IP address</h2>
     <p>Information from <a href="https://httpbin.org/ip">https://httpbin.org/ip</a>:</p>
-    <pre class="bg-slate-100 border font-mono p-4 rounded shadow w-full" id="ip_status"></pre>
-    <pre class="bg-slate-100 border font-mono p-4 rounded shadow w-full" id="ip_data"></pre>
+    <pre class="bg-slate-100 border font-mono p-4 rounded shadow w-full">{{ ip_status }}</pre>
+    <pre class="bg-slate-100 border font-mono p-4 rounded shadow w-full">{{ ip_data }}</pre>
   </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+/* vue */
+import { ref } from 'vue';
 
-onMounted(() => {
-  const ip_data = document.querySelector('#ip_data');
-  const ip_status = document.querySelector('#ip_status');
-  cordova.plugin.http.get('https://httpbin.org/ip', {}, {}, res => {
-    ip_status.innerText = res.status;
-    ip_data.innerText = res.data;
-  }, res => {
-    ip_status.innerText = res.status;
-    ip_data.innerText = res.error;
-  });
+/* refs */
+const ip_data = ref();
+const ip_status = ref();
+
+cordova.plugin.http.get('https://httpbin.org/ip', {}, {}, res => {
+  ip_status.value = res.status;
+  ip_data.value = res.data;
+}, res => {
+  ip_status.value = res.status;
+  ip_data.value = res.error;
 });
 </script>
