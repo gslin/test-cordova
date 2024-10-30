843d859f4f7d122a4722e7c899b324c4e6a00671
Gea-Suan Lin <gslin@gslin.org>
2024-10-30 16:46:28 +0800

    fix: disable warnings by giving definitions.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+
+const { DefinePlugin} = require('webpack');
 const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
@@ -14,5 +16,10 @@ module.exports = {
   },
   plugins: [
     new VueLoaderPlugin(),
+    new DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: true,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+    }),
   ],
 };
