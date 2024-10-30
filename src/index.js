88b4e1d79a7e3c010af04b48651119cb060caca3
Gea-Suan Lin <gslin@gslin.org>
2024-10-31 05:24:46 +0800

    chore: clean unsed codes.

--- src/index.js
+++ src/index.js
@@ -7,17 +7,6 @@ const main = () => {
   createApp(App)
     .use(router)
     .mount('#app');
-
-  /*
-  const el = document.querySelector('textarea');
-  el.innerText = 'fetching...';
-
-  cordova.plugin.http.sendRequest('https://www.example.com/', {}, res => {
-    el.innerText = 'OK\n' + res.data;
-  }, res => {
-    el.innerText = 'error\n' + res.error;
-  });
-  */
 };
 
 document.addEventListener('deviceready', main, false);
